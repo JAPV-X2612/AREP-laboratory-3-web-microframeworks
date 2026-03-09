50301a0028b122af8fb47511197868850faed293
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2026-03-09 01:14:03 -0500

    feat: Add HTTP response class

---
+++ src/main/java/edu/eci/arep/web/framework/Response.java
@@ -0,0 +1,11 @@
+package edu.eci.arep.web.framework;
+
+/**
+ * Placeholder representing an HTTP response within the framework's lambda handlers.
+ *
+ * @author Jesús Pinzón
+ * @version 1.0
+ * @since 2026-02-24
+ */
+public class Response {
+}
